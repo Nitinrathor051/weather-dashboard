6c98ae8af31af739651703897e4ad824d964604a
Nitin Rathor <nitinrathor0505feaid@gmail.com>
2026-05-21 08:32:53 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,37 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
+
   const token = localStorage.getItem("token");
 
   return (
     <Routes>
+
       {/* AUTH ROUTES */}
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route
+        path="/"
+        element={<Login />}
+      />
+
+      <Route
+        path="/register"
+        element={<Register />}
+      />
 
       {/* PROTECTED ROUTE */}
       <Route
         path="/dashboard"
-        element={token ? <Dashboard /> : <Navigate to="/" />}
+        element={
+          token
+            ? <Dashboard />
+            : <Navigate to="/" />
+        }
       />
+
     </Routes>
   );
 }
